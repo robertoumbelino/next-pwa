e8ec686e84d89ca24141c8582ce327a0794c9582
Roberto Umbelino <beto.umbelino@gmail.com>
2021-06-19 17:50:43 -0300

    feat: added logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 /**
  * Components.
  */
 import { Flex } from '~/components/Page/components/Flex'
 import { Typography } from '~/components/Typography/components/Typography'
+
+/**
+ * Images.
+ */
 
 export default function Home() {
   return (
@@ -14,18 +19,34 @@ export default function Home() {
       </Head>
 
       <main>
-        <Flex justifyContent="center" flexDirection="column" height="100vh">
-          <Typography variant="h1" isWhite fontSize={3}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          height="100vh"
+        >
+          <Image
+            src="/images/rocket.png"
+            alt="Logo"
+            sizes="192px"
+            layout="fixed"
+            width={192}
+            height={192}
+          />
+
+          <Typography variant="h1" isWhite fontSize={5}>
             Welcome
           </Typography>
 
-          <Typography variant="p" isWhite>
-            This is a simple but effective PWA Next Boilerplate.
-          </Typography>
+          <Flex margin={{ top: 50 }} flexDirection="column">
+            <Typography variant="p" isWhite fontSize={2}>
+              This is a simple but effective PWA Next Boilerplate.
+            </Typography>
 
-          <Typography variant="p" isWhite>
-            I hope you like 👍
-          </Typography>
+            <Typography variant="p" isWhite fontSize={3}>
+              I hope you like 👍
+            </Typography>
+          </Flex>
         </Flex>
       </main>
     </div>
